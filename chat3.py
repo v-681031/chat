5c384bfa5d3fa921057b2175f1777cec02d19820
v-681031 <vincentyu1031@gmail.com>
2019-10-01 23:41:46 +0800

    update to use string list split

--- chat3.py
+++ chat3.py
@@ -6,14 +6,22 @@ with open('3.txt', 'r', encoding='utf-8-sig') as f:
 		lines.append(line.strip())
 for line in lines:
 	s = line.split(' ')
-	if 'Allen' in s[0]:
-		name = 'Allen'
+	time = s[0][:5]
+	name = s[0][5:]
+	if name == 'Allen':
 		for allen in s[1:]:
 			allen_W_C += len(allen)
-	elif 'Viki' in s[0]:
-		name = 'Viki'
+	elif name == 'Viki':
 		for viki in s[1:]:
 			viki_W_C += len(viki)
+	#if 'Allen' in s[0]:
+	#	name = 'Allen'
+	#	for allen in s[1:]:
+	#		allen_W_C += len(allen)
+	#elif 'Viki' in s[0]:
+	#	name = 'Viki'
+	#	for viki in s[1:]:
+	#		viki_W_C += len(viki)
 print('Allen wording :', allen_W_C)
 print('Viki wording :', viki_W_C)
 
